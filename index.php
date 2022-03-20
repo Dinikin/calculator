d1a6f6a477c2e491f6f0fce96872379dde78de9f
Tema <artvor2005@yandex.ru>
2022-03-20 19:29:17 +0400

    fix

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 
 ?>
 <form action="" method="post">
-    <input type="textbox" name="first" requqred>
+    <input type="textbox" name="first" required>
 
     <select name="operation" required>
-        <option value="+"+</option>
+        <option value="+">+</option>
 <option value="-">-</option>      
      <option value="/">/</option>
      <option value="*">*</option>
-    <option value="^"+</option>
+    <option value="^">^</option>
     </select>
 
 <input type="textbox" name="second" required>
