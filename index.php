5f6589a23f34cb185e0dbe016b5cb94993289243
Tema <artvor2005@yandex.ru>
2022-03-20 23:13:49 +0400

    fix

--- index.php
+++ index.php
@@ -1,44 +1,67 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+	<meta charset = "UTF-8">
+	<link rel="stylesheet" href="styles.css" type="text/css" />
+</head>
+<body>
+	<h1>Калькулятор</h1>
+	<form action='' method='post' class="calculate-form">
+		<input type="text" name="number1" class="numbers" placeholder="Первое число">
+		<select class="operations" name="operation">
+			<option value='plus'>+</option>
+			<option value='minus'>-</option>
+			<option value="multiply">*</option>
+			<option value="divide">/</option>
+		</select>
+		<input type="text" name="number2" class="numbers" placeholder="Второе число">
+		
+		<input class="submit_form" type="submit" name="submit" value="Получить ответ"> 
+	</form>
+</body>
+</html>
+
 <?php
-    if($_POST['submit'])
-    {
-        $a= (int)$_POST['first'];
-        $b= (int)$_POST['second'];
-        $p= $_POST['operation'];
-        $c;
-        if($p=='+')
-                $c = $a + $b;
-        else if ($p=='-')
-                $c = $a - $b; 
-        else if ($p=='*')
-                $c = $a * $b;
-        else if ($p=='/')
-        {
-            if($b==0)
-                 echo "You can not by zero";
-            else $c = $a / $b;
-        }
-        else $c = pow($a, $b);
+if(isset($_POST['submit'])){
+	$number1 = $_POST['number1'];
+	$number2 = $_POST['number2'];
+	$operation = $_POST['operation'];
+	
+	// Валидация
+	if(!$operation || (!$number1 && $number1 != '0') || (!$number2 && $number2 != '0')) {
+		$error_result = 'Не все поля заполнены';
+
+	}
+    else {
+	    
+		if(!is_numeric($number1) || !is_numeric($number2)){
+			$error_result = "Операнды должны быть числами";
+		}
+		else 
+        switch($operation){
+			case 'plus':
+			    $result = $number1 + $number2;
+			    break;
+			case 'minus':
+			    $result = $number1 - $number2;
+			    break;
+			case 'multiply':
+			    $result = $number1 * $number2;
+			    break;
+			case 'divide':
+			    if( $number2 == '0')
+			    	$error_result = "На ноль делить нельзя!";
+			    else
+			       $result = $number1 / $number2;
+			    break;    
+		}
+	    
+	}
+    if(isset($error_result)){
+    	echo "<div class='error-text'>Ошибка: $error_result</div>";
+    }	
+    else {
+	    echo "<div class='answer-text'>Ответ: $result</div>";
     }
-
-?>
-<form action="" method="post">
-    <input type="textbox" name="first" required>
-
-    <select name="operation" required>
-        <option value="+">+</option>
-        <option value="-">-</option>      
-        <option value="/">/</option>
-        <option value="*">*</option>
-        <option value="^">^</option>
-    </select>
-
-    <input type="textbox" name="second" required>
-</form>
-
-    <input type="textbox" name="second" required>
-
-    <input type="submit" name="submit">
-</form>
-<?php
-        echo $c;
+}
 ?>
